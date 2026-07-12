462145ff4cd9c54407441160807a69024a5dfa01
Rushi <bhattrushi921@gmail.com>
2026-07-12 23:42:53 +0530

    Setting up the server and checking Whether Api key is activated or not.

---
+++ server/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+
+const API_KEY = process.env.OPENWEATHER_API_KEY;
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+//CUrrent Weather route
+app.get('/api/weather', async (req, res) => {
+    const { city } = req.query;
+    if (!city) return res.status(400).json({ error: "City is required!" });
+
+    try {
+        const response = await axios.get(`${BASE_URL}/weather`, {
+            params: { q: city, appid: API_KEY, units: 'metric' }
+        });
+        res.json(response.data);
+    } catch (err) {
+        if (err.response?.status === 404) {
+            return res.status(404).json({ error: "City  not found." });
+        }
+        res.status(500).json({ error: "Something went wrong." });
+    }
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, (req, res) => {
+    console.log(`Server is running on ${port}`);
+
+});
